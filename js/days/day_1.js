2a8e8d355f8bbc7f44fd97e4468693fee6593041
ShiguFan <frnzmnss@gmail.com>
2024-01-02 17:16:20 +0800

    typo

--- js/days/day_1.js
+++ js/days/day_1.js
@@ -3,7 +3,7 @@ registerDay(1,
     "title" : "Minepersvee 1 - Largely Against Following You",
     "desc" : "The first Minepersvee! Woo!<br><br>"
            + "When Day 61 began, my first custom variant was just a simple one featuring just Large and Anti Mines. It was nothing special.<br>"
-		   + "But today, we're adding a new entity to the mix, the Shoop! It's an anti-Sheep so whatever the direction the sheep moves, the shoop moveas the <b>opposite</b> direction.<br>"
+		   + "But today, we're adding a new entity to the mix, the Shoop! It's an anti-Sheep so whatever the direction the sheep moves, the shoop moves the <b>opposite</b> direction.<br>"
 		   + "Also, to minimize guessing, I made the Anti Mines <b>double</b> so yeah.",
     "mines" : {
         "RX" : 30,
